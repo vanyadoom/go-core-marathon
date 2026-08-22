0926f7293aebdbc77bcc97be6f6e74d7a7029640
vanyadoom <itworkhollow@gmail.com>
2026-08-22 16:09:31 +0300

    feat: complete step 10 with discount function and add gitignore shield

--- main.go
+++ main.go
@@ -2,19 +2,26 @@ package main
 
 import "fmt"
 
-func main() {
-	client1 := CalculateDelivery(2.5)
-	client2 := CalculateDelivery(10.0)
-	client3 := CalculateDelivery(0.5)
-
-	fmt.Printf("Первый клиент: %.2f\n", client1)
-	fmt.Printf("Второй клиент: %.2f\n", client2)
-	fmt.Printf("Третий клиент: %.2f\n", client3)
+func ApplyDiscount(amount, discount float64) (float64, bool) {
+	if discount < 0 || discount > 100.0 {
+		return amount, false
+	}
+	finalPrice := amount - (amount * (discount / 100.0))
+	return finalPrice, true
 }
 
-func CalculateDelivery(weight float64) float64 {
+func main() {
+	price1, ok1 := ApplyDiscount(5000.0, 15.0)
+	price2, ok2 := ApplyDiscount(2500.0, 120.0)
 
-	BasePrice := 300.0 + (weight * 50.5)
-
-	return BasePrice
+	if ok1 {
+		fmt.Printf("Покупка 1. Купон успешно применен! Финальная цена: %.2f\n", price1)
+	} else {
+		fmt.Printf("Покупка 1. Ошибка: Неверный процент скидки! Цена без изменений: %.2f\n", price1)
+	}
+	if ok2 {
+		fmt.Printf("Покупка 2. Купон успешно применен! Финальная цена: %.2f\n", price2)
+	} else {
+		fmt.Printf("Покупка 2. Ошибка: Неверный процент скидки! Цена без изменений: %.2f\n", price2)
+	}
 }
