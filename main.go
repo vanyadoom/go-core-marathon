7675f94f1cf286ab2b64011f737e91e279f4ba4d
vanyadoom <itworkhollow@gmail.com>
2026-08-24 18:46:36 +0300

    feat: complete step 21 - implement first structure method with value receiver

--- main.go
+++ main.go
@@ -4,24 +4,16 @@ import (
 	"fmt"
 )
 
-type Asset struct {
-	Ticker   string
-	ValueUSD float64
+type User struct {
+	Name  string
+	Coins int
+}
+
+func (u User) Showprofile() {
+	fmt.Printf("Пользователь: %s, Баланс: %d BTC\n", u.Name, u.Coins)
 }
 
 func main() {
-
-	portfolio := []Asset{
-		{Ticker: "BTC", ValueUSD: 1200.50},
-		{Ticker: "ETH", ValueUSD: 450.00},
-		{Ticker: "SOL", ValueUSD: 150.25},
-	}
-	totalPortfolioSum := 0.0
-
-	for _, item := range portfolio {
-		totalPortfolioSum += item.ValueUSD
-	}
-
-	fmt.Printf("Стоимость портфеля: %.2f\n", totalPortfolioSum)
-
+	vanya := User{Name: "Ваня", Coins: 3}
+	vanya.Showprofile()
 }
