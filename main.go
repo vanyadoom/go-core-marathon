956b6a9d23521cf4c1f84fb14671a99663ed076f
vanyadoom <itworkhollow@gmail.com>
2026-08-27 23:23:27 +0300

    feat: complete step 28 - implement empty interface any slice for dynamic storage

--- main.go
+++ main.go
@@ -2,38 +2,13 @@ package main
 
 import "fmt"
 
-type TaxCalculator interface {
-	CalculateTax() float64
-}
-
-type Wallet struct {
-	Balance float64
-}
-
-type CompanyAccount struct {
-	Profit float64
-}
-
-func (w Wallet) CalculateTax() float64 {
-	return w.Balance * 0.13
-}
-
-func (c CompanyAccount) CalculateTax() float64 {
-	return c.Profit * 0.20
-}
-
 func main() {
 
-	totalTax := 0.0
+	storage := []any{"Etherium", 2026, 3.14}
 
-	assets := []TaxCalculator{
-		Wallet{Balance: 2000.0},
-		CompanyAccount{Profit: 5000.0},
+	for _, data := range storage {
+
+		fmt.Println(data)
+
 	}
-
-	for _, asset := range assets {
-		totalTax += asset.CalculateTax() // 🟢 Automatically calls the correct method version!
-	}
-
-	fmt.Printf("%.2f\n", totalTax)
 }
