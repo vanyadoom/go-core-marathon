e38ea4e2d343f5102d93dc6b5263399aeb5e9797
vanyadoom <itworkhollow@gmail.com>
2026-08-27 18:58:30 +0300

    feat: complete step 27 - implement polymorphic interface slices with dynamic dispatch

--- main.go
+++ main.go
@@ -3,20 +3,37 @@ package main
 import "fmt"
 
 type TaxCalculator interface {
-	CalculateTax() float64 // Только сигнатура: имя, аргументы и что возвращает
+	CalculateTax() float64
 }
 
 type Wallet struct {
 	Balance float64
 }
 
+type CompanyAccount struct {
+	Profit float64
+}
+
 func (w Wallet) CalculateTax() float64 {
 	return w.Balance * 0.13
 }
 
-func main() {
-	myWallet := Wallet{Balance: 1000.0}
-	var calc TaxCalculator = myWallet
+func (c CompanyAccount) CalculateTax() float64 {
+	return c.Profit * 0.20
+}
 
-	fmt.Printf("Сумма налога: %.2f USD\n", calc.CalculateTax())
+func main() {
+
+	totalTax := 0.0
+
+	assets := []TaxCalculator{
+		Wallet{Balance: 2000.0},
+		CompanyAccount{Profit: 5000.0},
+	}
+
+	for _, asset := range assets {
+		totalTax += asset.CalculateTax() // 🟢 Automatically calls the correct method version!
+	}
+
+	fmt.Printf("%.2f\n", totalTax)
 }
