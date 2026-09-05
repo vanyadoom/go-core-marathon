0cd4f814a7ebdaf6e4611c27d86fbafeda2032eb
vanyadoom <itworkhollow@gmail.com>
2026-09-05 20:48:49 +0300

    feat: complete step 48 - implement resilient concurrency timeouts via select and time After

--- main.go
+++ main.go
@@ -5,28 +5,20 @@ import (
 	"time"
 )
 
-func FetchBinance(ch chan string) {
-	time.Sleep(200 * time.Millisecond)
-	ch <- "Binance: BTC = 64500 USD"
-}
-func FetchBybit(ch chan string) {
-	time.Sleep(50 * time.Millisecond)
-	ch <- "Bybit: BTC = 64480 USD"
+func FetchSlowBybit(ch chan string) {
+	time.Sleep(500 * time.Millisecond)
+	ch <- "Bybit: Успешный ответ!"
 }
 
 func main() {
-	binanceChan := make(chan string)
 	bybitChan := make(chan string)
-
-	go FetchBinance(binanceChan)
-	go FetchBybit(bybitChan)
-
-	fmt.Println("Поток main: Запускаю опрос бирж наперегонки...")
+	go FetchSlowBybit(bybitChan)
+	fmt.Println("Поток main: Запрашиваю данные у Bybit со встроенной защитой 100 мс...")
 
 	select {
-	case res := <-binanceChan:
-		fmt.Println("Победил агрегатор!", res)
 	case res := <-bybitChan:
-		fmt.Println("Победил агрегатор!", res)
+		fmt.Println("Успех!", res)
+	case <-time.After(100 * time.Millisecond):
+		fmt.Println("❌ Тайм-аут! Биржа Bybit зависла, сбрасываем соединение.")
 	}
 }
